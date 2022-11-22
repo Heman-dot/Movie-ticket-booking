6e15d488f691237761f4b37476b5da1324313d28
Heman-dot <hemannarayanan@gmail.com>
2022-11-22 23:33:42 +0530

    Welcome page

--- index.c
+++ index.c
@@ -11,7 +11,7 @@ int main()
         scanf("%d",&x);
         if(x==3)
         {
-            printf("Thank you!!!Cum again!!haaa");
+            printf("Thank you!!!");
             break;
         }
         if((x<1)||(x>3))
