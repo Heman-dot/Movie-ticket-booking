6ce3b29438d30e98cce38b5115a7e701033a8891
Heman-dot <hemannarayanan@gmail.com>
2022-11-22 23:05:15 +0530

    Welcome page

--- index.c
+++ index.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+
 int main()
 {
-    printf("hello");
-    printf("hello universe");
+    int x;
+    while(x)
+    {
+        printf(" Welcome To Movie ticket Booking\n");
+        printf("1-Ticket Booking\n2-Show Tickets\n3-Log out\n");
+        printf("Enter you choice:");
+        scanf("%d",&x);
+        if(x==3)
+        {
+            printf("Thank you!!!Cum again!!haaa");
+            break;
+        }
+        if((x<1)||(x>3))
+        {
+            printf("Enter a number among the given numbers\n");
+            continue;
+        }
+        
+    }
+    return 0;
 }
